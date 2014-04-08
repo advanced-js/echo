de7aab17c79eab6c023343eccb233eaba9773d5d
msav364 <msav364@earthlink.net>
2014-04-08 19:53:57 -0400

    Echo change

--- echo.js
+++ echo.js
@@ -1,11 +1,6 @@
-function echo(string1,string2, string3) {
- myAry = new Array();
- myAry[0] = string1;
-  myAry[1] = string2;
-  myAry[2] = string3;
- for (i=0; i < myAry.length; i++) {
-   console.log(myAry[i]);
- }
+function echo() {
+for (i=0;i<arguments.length;i++)
+console.log(arguments[i]);
  }
 
 echo();
