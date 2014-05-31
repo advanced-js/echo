dc6d68c99a484d1fe110b41037ba440fd2c80f62
Vu Nguyen <nnvu@outlook.com>
2014-05-31 12:19:33 -0400

    echo function implemented
    first try

--- echo.js
+++ echo.js
@@ -1,5 +1,9 @@
-function echo(){
-  // ...
+function echo() {
+    if(arguments.length) {
+        for (var i = 0; i < arguments.length; i++) {
+            console.log(arguments[i]);
+        }
+    }
 }
 
 echo();
