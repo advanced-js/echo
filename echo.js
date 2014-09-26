12115bb8e910db504f3c62c17542c5be784ffdb2
alexoviedo <alexoviedo999@gmail.com>
2014-09-25 20:42:57 -0400

    echo excersise

--- echo.js
+++ echo.js
@@ -3,5 +3,15 @@ function echo(){
 }
 
 echo();
+
+function echo(word){
+	console.log(word)
+}
 echo('bla');
+
+function echo(arg1, arg2, arg3){
+	console.log(arg1);
+	console.log(arg2);
+	console.log(arg3);
+}
 echo('foo', 'bar', 'baz');
