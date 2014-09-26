00023f83ee016882c6a32a125d9d6e09992bb389
DJ Rosenbaum <dave@djrconcepts.com>
2014-09-25 20:47:40 -0400

    echo function arguments to browser console

--- echo.js
+++ echo.js
@@ -11,7 +11,15 @@ echo('foo', 'bar', 'baz');
 
 //Modify echo.js to print out each argument to the echo() function on a new line.
 function echo() {
-    console.log(arguments);
+
+    //determine number of arguments passed into echo function
+    var number_of_arguments = arguments.length;
+
+    //loop through each argument and log to the console
+    for ( var i = 0; i<number_of_arguments; i++ ) {
+        console.log( arguments[i] );
+    }
+
 }
 
 echo();
