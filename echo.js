268b2507b94477e1a38ed8b72c3e47b59ace5c7c
DJ Rosenbaum <dave@djrconcepts.com>
2014-09-25 20:40:35 -0400

    echo arguments

--- echo.js
+++ echo.js
@@ -1,7 +1,21 @@
+/*
 function echo(){
   // ...
 }
 
 echo();
 echo('bla');
+echo('foo', 'bar', 'baz');
+*/
+
+
+//Modify echo.js to print out each argument to the echo() function on a new line.
+function echo() {
+    console.log(arguments);
+}
+
+echo();
+
+echo('bla');
+
 echo('foo', 'bar', 'baz');
