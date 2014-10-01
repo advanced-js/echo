04aa98e3c4eec21ca975267822802ef3a4afd5dd
Jen <jennifer.calloway@refinery29.com>
2014-09-30 21:04:31 -0400

    edited echo function

--- echo.js
+++ echo.js
@@ -1,10 +1,6 @@
-function echo(arg1, arg2, arg3){
-  if (arguments.length === 0) {
-    console.log('');
-  } else if (arguments.length === 1) {
-    console.log(arg1);
-  } else {
-    console.log(arg1 + '\n' + arg2 + '\n' + arg3);
+function echo() {
+  for (var i=0; i < arguments.length; i++) {
+    console.log(arguments[i]);
   }
 }
 
