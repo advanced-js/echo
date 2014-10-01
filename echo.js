d5f2dbea92103c6106c55360c40d20d900a74e6e
Jen <jennifer.calloway@refinery29.com>
2014-09-30 20:52:16 -0400

    Echo function

--- echo.js
+++ echo.js
@@ -1,5 +1,11 @@
-function echo(){
-  // ...
+function echo(arg1, arg2, arg3){
+  if (arguments.length === 0) {
+    console.log('');
+  } else if (arguments.length === 1) {
+    console.log(arg1);
+  } else {
+    console.log(arg1 + '\n' + arg2 + '\n' + arg3);
+  }
 }
 
 echo();
