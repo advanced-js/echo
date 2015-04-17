b93160ca330f88fa16a44c9ba763643414633ff0
Kobe Osei-Akoto <kobeosei-akoto@172-30-49-40.DYNAPOOL.NYU.EDU>
2015-04-16 21:12:44 -0400

    Actually iterating

--- echo.js
+++ echo.js
@@ -1,11 +1,9 @@
 function echo() {
   for (var i = 0; i < arguments.length) {
-  	console.log(arguments + '\n');
+  	console.log(arguments[i]);
   }
 }
 
 echo();
 echo('bla');
 echo('foo', 'bar', 'baz');
-
-
