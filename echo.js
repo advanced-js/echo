96849f0240b08806af72ac7dffefda83a21322e9
Kobe Osei-Akoto <kobeosei-akoto@172-30-49-40.DYNAPOOL.NYU.EDU>
2015-04-16 21:11:49 -0400

    Complete echo exercise

--- echo.js
+++ echo.js
@@ -1,7 +1,11 @@
-function echo(){
-  // ...
+function echo() {
+  for (var i = 0; i < arguments.length) {
+  	console.log(arguments + '\n');
+  }
 }
 
 echo();
 echo('bla');
 echo('foo', 'bar', 'baz');
+
+
