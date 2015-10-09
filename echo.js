475585690c7bb9f1459930fcb738e3c7c26c4c0b
git-ebetts <ebettsmail@gmail.com>
2015-10-09 11:15:49 -0400

    Part 2 Solution: first commit

--- echo.js
+++ echo.js
@@ -1,9 +1,30 @@
-// PART 1:
-// echo();
-// should print nothing
+// // PART 1:
+// // echo();
+// // should print nothing
 
-function echo() {
-    console.log();
+// function echo() {
+//     console.log();
+// }
+
+// echo();
+
+//------------------------
+//PART 2:
+//echo('bla');
+// should print
+//
+// 'bla'
+
+function echo(x) {
+    console.log(x);
 }
 
-echo();
+echo('bla');
+
+//ALTERNATE SOLUTION:
+
+function echo() {
+    console.log(arguments[0]);
+}
+
+echo('bla');
