2221a5cd61b7c53f96919d0b5559eb5d17dcd67e
Eric <ericblood1@gmail.com>
2015-10-08 21:25:12 -0400

    Eric Blood - Echo Assignment

--- echo.js
+++ echo.js
@@ -1,6 +1,19 @@
-function echo(){
-  // ...
+var counter = 1;
+	
+function echo(){	
+	var emptyArg = true;
+    var html = "This is echo call number <b><i>" + counter + "</i></b> and the arguments are:<br />";
+	counter++;
+    for (var i=0; i < arguments.length; i++) {
+		emptyArg = false;
+        html += '<b>' + arguments[i] + '</b><br />';
+    }
+	if (emptyArg)
+		html += '<i>no arguments were provided</i>';
+    document.write(html + '<br /><br/>');   
 }
+
+
 
 echo();
 echo('bla');
