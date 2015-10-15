4bcc2e948a22604dfe6d3d8dcced153cfeb41d26
Eric <ericblood1@gmail.com>
2015-10-15 16:31:45 -0400

    remove doc writes

--- echo.js
+++ echo.js
@@ -1,16 +1,7 @@
-var counter = 1;
-	
 function echo(){	
-	var emptyArg = true;
-    var html = "This is echo call number <b><i>" + counter + "</i></b> and the arguments are:<br />";
-	counter++;
     for (var i=0; i < arguments.length; i++) {
-		emptyArg = false;
-        html += '<b>' + arguments[i] + '</b><br />';
+		console.log(arguments[i]);
     }
-	if (emptyArg)
-		html += '<i>no arguments were provided</i>';
-    document.write(html + '<br /><br/>');   
 }
 
 
