74c4008a8884c7293dede2dc230396931c7fe643
bluemittens505 <blaird2e@verizon.net>
2016-02-04 21:27:04 -0500

    Add code for echo function

--- echo.js
+++ echo.js
@@ -1,7 +1,10 @@
 function echo(){
-  // ...
+  for (var i = 0; i < arguments.length; i++) {
+  	document.write(arguments[i] + "<br>");
+  };
 }
 
 echo();
 echo('bla');
 echo('foo', 'bar', 'baz');
+console.log("testing");
