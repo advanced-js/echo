aec1a5dead844fa14bf0394666e9c94d2b1c1260
bluemittens505 <blaird2e@verizon.net>
2016-02-06 14:03:46 -0500

    some tweaks
    Move break before write of argument and remove console.log

--- echo.js
+++ echo.js
@@ -1,10 +1,9 @@
 function echo(){
   for (var i = 0; i < arguments.length; i++) {
-  	document.write(arguments[i] + "<br>");
+  	document.write("<br/>" + arguments[i]);
   };
 }
 
 echo();
 echo('bla');
 echo('foo', 'bar', 'baz');
-console.log("testing");
